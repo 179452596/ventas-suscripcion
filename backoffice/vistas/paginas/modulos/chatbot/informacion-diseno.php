9fb6bae53b004fb2bdf4eea66016288a43c7e084
oscartoloza <oscar.toloza.lemus@gmail.com>
2023-08-25 12:41:17 -0400

    primeros cambios

---
+++ backoffice/vistas/paginas/modulos/chatbot/informacion-diseno.php
@@ -0,0 +1,45 @@
+<div class="col-12 col-md-8">
+
+<section class="content-header">
+    
+    <div class="container-fluid">
+      <div class="row mb-4">
+        <div class="col-sm-6">
+          <h1>Informacion</h1>
+          <br>
+          <ol>		
+              <!--Texto para nombre del robot-->
+              <h6>Nombre del Robot</h3>
+              <input type="text" id="mensaje-widget" placeholder="Ej: richard">
+          </ol>
+
+          <ol>		
+            <!--RADIO 1-->
+            <h6>Icono del Widget</h3> <br>
+            <input type="radio" class="radio" value="" name="item" id="radio1">
+            <label class="label" for="radio1"> Español </label>&emsp;
+
+            <!--RADIO 2-->
+            <input type="radio" class="radio" value="" name="item" id="radio2">
+            <label class="label" for="radio2"> Ingles </label>&emsp;
+
+            <!--RADIO 3-->
+            <input type="radio" class="radio" value="" name="item" id="radio3">
+            <label class="label" for="radio3"> Aleman </label>
+          </ol>
+
+          <ol>
+            <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+            <label class="form-check-label">Option 1</label>
+            </div>
+          </ol>
+
+		</div>
+      </div>
+    </div><!-- /.container-fluid -->
+
+  </section>
+	
+</div>
+
