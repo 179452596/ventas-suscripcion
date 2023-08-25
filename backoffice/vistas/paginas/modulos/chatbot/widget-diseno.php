379d134c9ec8e09f368b3eeae144ae703166f8fe
Danykza Ossandon <danykza.ossandon.jaragmail.com>
2023-08-25 10:54:21 -0400

    Agregando administración de chatbot

---
+++ backoffice/vistas/paginas/modulos/chatbot/widget-diseno.php
@@ -0,0 +1,22 @@
+<div class="col-12 col-md-8">
+
+<section class="content-header">
+    
+    <div class="container-fluid">
+      <div class="row mb-2" style="text-align: center;">
+        <div class="col-sm-6">
+          <h1>Apariencia</h1>
+		  <br>
+		  	<ol>		
+			  <li class="btn btn-default"> <a href="#">Inicio</a></li>
+			</ol>
+
+		</div>
+
+      </div>
+    </div><!-- /.container-fluid -->
+
+  </section>
+	
+</div>
+
