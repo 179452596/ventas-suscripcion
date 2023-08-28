1f68c26ca9859e8fb1b8537c9619ea8d14e79f3b
Danykza Ossandon <danykza.ossandon.jaragmail.com>
2023-08-28 11:59:24 -0400

    cambios faltantes

--- backoffice/vistas/paginas/modulos/chatbot/informacion-diseno.php
+++ backoffice/vistas/paginas/modulos/chatbot/informacion-diseno.php
@@ -7,21 +7,32 @@
         <div class="col-sm-6">
           <h1>Informacion</h1>
           <br>
+          <ol> 
+          <div class="col-sm-6">
+            <label class="label"> Activar widget y chatbot </label>
+          </div>
+          <div class="col-sm-6">
+            <label class="switch">
+              <input type="checkbox">
+              <span class="slider round"></span>
+            </label>
+          </div>
+          </ol>
           <ol>		
               <!--Texto para nombre del robot-->
-              <h6>Nombre del Robot</h3>
+              <h6><i class="fa-solid fa-robot"></i> Nombre del Robot</h6>
               <input type="text" id="mensaje-widget" placeholder="Ej: richard">
           </ol>
-
+          <br>
           <ol>		
             <!--RADIO 1-->
-            <h6>Icono del Widget</h3> <br>
+            <h5><i class="fas fa-language fa-lg"></i> Idioma</h5>
             <input type="radio" class="radio" value="" name="item" id="radio1">
             <label class="label" for="radio1"> Español </label>&emsp;
 
             <!--RADIO 2-->
             <input type="radio" class="radio" value="" name="item" id="radio2">
-            <label class="label" for="radio2"> Ingles </label>&emsp;
+            <label class="label" for="radio2"> Inglés </label>&emsp;
 
             <!--RADIO 3-->
             <input type="radio" class="radio" value="" name="item" id="radio3">
@@ -29,13 +40,37 @@
           </ol>
 
           <ol>
-            <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
-            <label class="form-check-label">Option 1</label>
-            </div>
+              <h5> <i class="fa-regular fa-clock"></i> Horario Comercial</h5>
+                <div class="dbgCont">
+                    <input type="checkbox" id="Lunes" class="dbgCheck" checked/>
+                    <label for="Lunes">Lu</label>
+                </div>
+                <div class="dbgCont">
+                    <input type="checkbox" id="Martes" class="dbgCheck" checked/>
+                    <label for="Martes">Ma</label>
+                </div>
+                <div class="dbgCont">
+                    <input type="checkbox" id="Miercoles"  class="dbgCheck" checked/>
+                    <label for="Miercoles">Mi</label>
+                </div>
+                <div class="dbgCont">
+                    <input type="checkbox" id="Jueves"  class="dbgCheck" checked/>
+                    <label for="Jueves">Ju</label>
+                </div>
+                <div class="dbgCont">
+                    <input type="checkbox" id="Viernes"  class="dbgCheck" checked/>
+                    <label for="Viernes">Vi</label>
+                </div>
+                <div class="dbgCont">
+                    <input type="checkbox" id="Sabado"  class="dbgCheck" />
+                    <label for="Sabado">Sa</label>
+                </div>
+                <div class="dbgCont">
+                    <input type="checkbox" id="Domingo"  class="dbgCheck" />
+                    <label for="Domingo">Do</label>
+                </div>
           </ol>
 
-		</div>
       </div>
     </div><!-- /.container-fluid -->
 
